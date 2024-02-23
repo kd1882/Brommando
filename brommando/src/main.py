d856e89a235132c8fb11d76c254d770b905358b3
kd1882 <kd_112@protonmail.com>
2024-02-23 18:45:52 -0500

    Cleaned up code, removed old comments.

--- brommando/src/main.py
+++ brommando/src/main.py
@@ -1,9 +1,10 @@
 # src/main.py
 import pygame
 import settings
+# from assets_manager import AssetManager
 
 class Game:
-    def __init__(self):  # Corrected method name
+    def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
@@ -11,51 +12,15 @@ class Game:
 
     def game_loop(self):
         while self.running:
-            self.clock.tick(settings.FPS)  # Use self.clock
+            self.clock.tick(settings.FPS)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
 
-    def run(self):  # Implement the missing run method
+    def run(self): 
         self.game_loop()
 
 if __name__ == '__main__':
     game = Game()
     game.run()
-
-
-
-# from assets_manager import AssetManager
-
-# def main():
-#     pygame.init()
-#     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-#     clock = pygame.time.Clock()
-#     running = True
-#     #assets = AssetManager()
-
-#     # Example of loading assets
-#     #player_image = assets.load_image('player', 'player.png')
-#     #background_music = assets.load_sound('background', 'music.ogg')
-#     #game_font = assets.load_font('default', 'OpenSans-Bold.ttf', 24)
-
-#     # Play background music
-#     #background_music.play(-1)  # Loop indefinitely
-
-#     while running:
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 running = False
-
-#         #screen.fill((0, 0, 0))  # Clear screen
-
-#         # Game logic and rendering
-#         #screen.blit(player_image, (100, 100))  # Example of using an asset
-
-#         #pygame.display.flip()  # Update the display
-#         clock.tick(60)  # Maintain 60 FPS
-
-#     pygame.quit()
-
-
